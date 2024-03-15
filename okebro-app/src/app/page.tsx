cf06f0c39bd84632728116cd9fb8314300b83320
Tofan Akbar Al Fawzy <tofanakbar1234@gmail.com>
2024-03-15 20:14:32 +0700

    Update page.tsx

--- okebro-app/src/app/page.tsx
+++ okebro-app/src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find in-depth information about Oke Broo.
           </p>
         </a>
 
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about Oke Broo with&nbsp;quizzes!
           </p>
         </a>
 
@@ -87,7 +87,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            Explore starter templates Oke Browww.
           </p>
         </a>
 
